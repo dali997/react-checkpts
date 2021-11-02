c8035214853c4c75f567c525867987ca3f56dba6
dali997 <dalicherif1818@gmail.com>
2021-11-02 12:35:15 +0100

    starting new react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import FormValidation from './Component/Form';
+import Dali from './Component/dali/Dali';
+import './App.css'
+function App() {
+  return (
+    <div > 
+      <Dali />
+    <FormValidation />
+    </div>
+  );
+}
+export default App;
+
